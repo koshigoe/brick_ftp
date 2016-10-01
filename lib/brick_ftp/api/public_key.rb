3b4f6c7779f0bddb416ea895c256a809d434460d
koshigoe <koshigoeb@gmail.com>
2016-10-02 01:59:11 +0900

    Create public key.

--- lib/brick_ftp/api/public_key.rb
+++ lib/brick_ftp/api/public_key.rb
@@ -6,6 +6,12 @@ module BrickFTP
         data.map { |x| new(x.symbolize_keys) }
       end
 
+      def self.create(user_id, title:, public_key:)
+        params = { title: title, public_key: public_key }
+        data = BrickFTP::HTTPClient.new.post("/api/rest/v1/users/#{user_id}/public_keys.json", params: params)
+        new(data.symbolize_keys)
+      end
+
       attr_reader :id, :title, :fingerprint, :created_at
 
       def initialize(id:, title:, fingerprint:, created_at:)
--- spec/brick_ftp/api/public_key_spec.rb
+++ spec/brick_ftp/api/public_key_spec.rb
@@ -32,4 +32,35 @@ RSpec.describe BrickFTP::API::PublicKey, type: :lib do
       expect(public_keys.first.created_at).to eq '2016-09-30T01:14:26-04:00'
     end
   end
+
+  describe '.create' do
+    subject { described_class.create(1, params) }
+
+    let(:params) { { title: 'new key', public_key: public_key } }
+    let(:public_key) { 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0bhSqoXErPk7tvtmjw2xD6+ql/yg/0I3K+jC+q2nkmrg34gU9tk9eG38wuALMT4MyP3iW9os3c1A4MwBZRnmuBAEBvmHMFKvEvWE2txxBZ9FZJU2KOg0kWnOFrPbIk1Eu+ndkbaJg8T2D4kJXfKtx6C/ArYlJqkA24wlxA/kTkwzJFFs9CqpYJPEVkut+JoecfW7LKiPIxEt+8wLuKLAwz83mtzW/s3oma0H5z5z5TAP96Yim9bGAL+AgY0qsJZKhzc0fcQ0B80BDO7XbdOipffCRmb0UuiK4OC5Z7va1XD5PkAIo143eg4mU57RskfXbHR5NLrt2ozdXCXLmavrh' }
+
+    before do
+      public_key = {
+        "id" => 1217,
+        "title" => "TEST",
+        "fingerprint" => "1b:11:87:39:5c:88:05:ec:cc:41:33:90:b0:70:e2:8b",
+        "created_at" => "2016-09-30T01:14:26-04:00"
+      }
+      stub_request(:post, 'https://koshigoe.brickftp.com/api/rest/v1/users/1/public_keys.json')
+        .with(body: params.to_json, basic_auth: ['xxxxxxxx', 'x'])
+        .to_return(status: 201, body: public_key.to_json)
+    end
+
+    it 'return Array of BrickFTP::API::PublicKey' do
+      is_expected.to be_an_instance_of(BrickFTP::API::PublicKey)
+    end
+
+    it 'set attributes' do
+      public_key = subject
+      expect(public_key.id).to eq 1217
+      expect(public_key.title).to eq 'TEST'
+      expect(public_key.fingerprint).to eq '1b:11:87:39:5c:88:05:ec:cc:41:33:90:b0:70:e2:8b'
+      expect(public_key.created_at).to eq '2016-09-30T01:14:26-04:00'
+    end
+  end
 end
