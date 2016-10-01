57719767ef12a4c196bd8f2dbbe97d918e39350b
koshigoe <koshigoeb@gmail.com>
2016-10-02 00:46:18 +0900

    Create new user.

--- lib/brick_ftp/api/user.rb
+++ lib/brick_ftp/api/user.rb
@@ -10,6 +10,15 @@ module BrickFTP
         data.empty? ? nil : new(data.symbolize_keys)
       end
 
+      def self.create(username:, password: nil, name: nil, email: nil, notes: nil, group_ids: nil,
+                     ftp_permission: nil, web_permission: nil, sftp_permission: nil, dav_permission: nil,
+                     restapi_permission: nil, attachments_permission: nil, self_managed: nil, require_password_change: nil,
+                     allowed_ips: nil, user_root: nil, grant_permission: nil, ssl_required: nil, authentication_method: nil)
+        params = Hash[binding.local_variables.map { |_| [_, binding.local_variable_get(_)] }].reject { |_, v| v.nil? }
+        data = BrickFTP::HTTPClient.new.post('/api/rest/v1/users.json', params: params)
+        new(data.symbolize_keys)
+      end
+
       attr_reader :id,
                   :username,
                   :password,
@@ -29,32 +38,20 @@ module BrickFTP
                   :user_root,
                   :grant_permission,
                   :ssl_required,
-                  :authentication_method
+                  :authentication_method,
+                  :last_login_at,
+                  :time_zone,
+                  :language,
+                  :site_admin
 
       def initialize(id: nil, username: nil, password: nil, name: nil, email: nil, notes: nil, group_ids: nil,
                      ftp_permission: nil, web_permission: nil, sftp_permission: nil, dav_permission: nil,
                      restapi_permission: nil, attachments_permission: nil, self_managed: nil, require_password_change: nil,
-                     allowed_ips: nil, user_root: nil, grant_permission: nil, ssl_required: nil, authentication_method: nil)
-        @id = id
-        @username = username
-        @password = password
-        @name = name
-        @email = email
-        @notes = notes
-        @group_ids = group_ids
-        @ftp_permission = ftp_permission
-        @web_permission = web_permission
-        @sftp_permission = sftp_permission
-        @dav_permission = dav_permission
-        @restapi_permission = restapi_permission
-        @attachments_permission = attachments_permission
-        @self_managed = self_managed
-        @require_password_change = require_password_change
-        @allowed_ips = allowed_ips
-        @user_root = user_root
-        @grant_permission = grant_permission
-        @ssl_required = ssl_required
-        @authentication_method = authentication_method
+                     allowed_ips: nil, user_root: nil, grant_permission: nil, ssl_required: nil, authentication_method: nil,
+                     last_login_at: nil, time_zone: nil, language: nil, site_admin: nil)
+        binding.local_variables.map do |_|
+          instance_variable_set(:"@#{_}", binding.local_variable_get(_))
+        end
       end
     end
   end
--- spec/brick_ftp/api/user_spec.rb
+++ spec/brick_ftp/api/user_spec.rb
@@ -145,4 +145,106 @@ RSpec.describe BrickFTP::API::User, type: :lib do
       end
     end
   end
+
+  describe '.create' do
+    subject { described_class.create(params) }
+
+    context 'success' do
+      let(:params) do
+        {
+          username: "johndoe",
+          password: "doejohn123",
+          name: "John Doe",
+          email: "john@example.com",
+          notes: "CTO",
+          group_ids: "3,4",
+          ftp_permission: true,
+          web_permission: true,
+          sftp_permission: true,
+          dav_permission: true,
+          restapi_permission: true,
+          attachments_permission: false,
+          self_managed: true,
+          require_password_change: false,
+          allowed_ips: "",
+          user_root: "",
+          grant_permission: "read"
+        }
+      end
+
+      before do
+        body = {
+          "id" => 125108,
+          "username" => "johndoe",
+          "authentication_method" => "password",
+          "last_login_at" => nil,
+          "name" => "John Doe",
+          "email" => "john@example.com",
+          "notes" => "CTO",
+          "group_ids" => "3,4",
+          "ftp_permission" => true,
+          "web_permission" => true,
+          "sftp_permission" => true,
+          "dav_permission" => true,
+          "restapi_permission" => true,
+          "attachments_permission" => true,
+          "self_managed" => true,
+          "require_password_change" => false,
+          "allowed_ips" => nil,
+          "user_root" => nil,
+          "time_zone" => "Eastern Time (US & Canada)",
+          "language" => "",
+          "ssl_required" => "use_system_setting",
+          "site_admin" => false,
+        }
+        stub_request(:post, 'https://koshigoe.brickftp.com/api/rest/v1/users.json')
+          .with(body: params.to_json, basic_auth: ['xxxxxxxx', 'x'])
+          .to_return(status: 201, body: body.to_json)
+      end
+
+      it 'return instance of User' do
+        is_expected.to be_an_instance_of BrickFTP::API::User
+      end
+
+      it 'set attributes' do
+        user = subject
+        expect(user.id).to eq 125108
+        expect(user.username).to eq "johndoe"
+        expect(user.authentication_method).to eq "password"
+        expect(user.last_login_at).to eq nil
+        expect(user.name).to eq "John Doe"
+        expect(user.email).to eq "john@example.com"
+        expect(user.notes).to eq "CTO"
+        expect(user.group_ids).to eq "3,4"
+        expect(user.ftp_permission).to eq true
+        expect(user.web_permission).to eq true
+        expect(user.sftp_permission).to eq true
+        expect(user.dav_permission).to eq true
+        expect(user.restapi_permission).to eq true
+        expect(user.attachments_permission).to eq true
+        expect(user.self_managed).to eq true
+        expect(user.require_password_change).to eq false
+        expect(user.allowed_ips).to eq nil
+        expect(user.user_root).to eq nil
+        expect(user.time_zone).to eq "Eastern Time (US & Canada)"
+        expect(user.language).to eq ""
+        expect(user.ssl_required).to eq "use_system_setting"
+        expect(user.site_admin).to eq false
+      end
+    end
+
+    context 'failure' do
+      let(:params) { { username: 'koshigoe' } }
+
+      before do
+        stub_request(:post, 'https://koshigoe.brickftp.com/api/rest/v1/users.json')
+          .with(basic_auth: ['xxxxxxxx', 'x'])
+          .to_return(status: 500, body: { 'error' => 'xxxxxxxx', 'http-code' => '500' }.to_json)
+      end
+
+      it 'raise BrickFTP::HTTPClient::Error' do
+        expect { subject }.to raise_error BrickFTP::HTTPClient::Error
+      end
+    end
+  end
 end
