50c491f11a3befbe609c51cdffc5d13a3833a2e5
koshigoe <koshigoeb@gmail.com>
2016-10-02 16:06:19 +0900

    Add Site History API

---
+++ lib/brick_ftp/api/history/site.rb
@@ -0,0 +1,21 @@
+module BrickFTP
+  module API
+    module History
+      class Site < BrickFTP::API::Base
+        define_api :index, '/api/rest/v1/history.json'
+        define_readonly_attributes :id,
+                                   :when,
+                                   :user_id,
+                                   :username,
+                                   :action,
+                                   :failure_type,
+                                   :path,
+                                   :source,
+                                   :destination,
+                                   :targets,
+                                   :ip,
+                                   :interface
+      end
+    end
+  end
+end
