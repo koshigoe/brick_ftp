c2145f93ec9bd2df2681bb6ef05bafcc6e356c9d
koshigoe <koshigoeb@gmail.com>
2016-10-03 16:37:06 +0900

    Add user methods to API Client.

--- lib/brick_ftp/client.rb
+++ lib/brick_ftp/client.rb
@@ -11,5 +11,38 @@ module BrickFTP
     def logout
       BrickFTP::API::Authentication.logout
     end
+
+    # List all users on the current site.
+    # @return [Array] array of BrickFTP::API::User
+    def list_users
+      BrickFTP::API::User.all
+    end
+
+    # Show a single user.
+    # @return [BrickFTP::API::User] user object.
+    def show_user(id)
+      BrickFTP::API::User.find(id)
+    end
+
+    # Create a new user on the current site.
+    # @param attributes [Hash] User's attributes.
+    def create_user(attributes)
+      BrickFTP::API::User.create(attributes)
+    end
+
+    # Update an existing user.
+    # @param user [BrickFTP::API::User] user object.
+    # @param attributes [Hash] User's attributes.
+    # @return [BrickFTP::API::User] user object.
+    def update_user(user, attributes)
+      user.update(attributes)
+    end
+
+    # Delete a user.
+    # @param user [BrickFTP::API::User] user object.
+    # @return [Boolean] return true.
+    def delete_user(user)
+      user.destroy
+    end
   end
 end
--- spec/brick_ftp/client_spec.rb
+++ spec/brick_ftp/client_spec.rb
@@ -16,4 +16,46 @@ RSpec.describe BrickFTP::Client, type: :lib do
       end
     end
   end
+
+  describe 'User' do
+    describe '#list_users' do
+      it 'delegate BrickFTP::API::User.all' do
+        expect(BrickFTP::API::User).to receive(:all)
+        described_class.new.list_users
+      end
+    end
+
+    describe '#show_user' do
+      let(:id) { 1 }
+      it 'delegate BrickFTP::API::User.find' do
+        expect(BrickFTP::API::User).to receive(:find).with(id)
+        described_class.new.show_user(id)
+      end
+    end
+
+    describe '#create_user' do
+      let(:attributes) { {} }
+      it 'delegate BrickFTP::API::User.create' do
+        expect(BrickFTP::API::User).to receive(:create).with(attributes)
+        described_class.new.create_user(attributes)
+      end
+    end
+
+    describe '#update_user' do
+      let(:user) { BrickFTP::API::User.new(id: 1) }
+      let(:attributes) { {} }
+      it 'delegate BrickFTP::API::User#update' do
+        expect(user).to receive(:update).with(attributes)
+        described_class.new.update_user(user, attributes)
+      end
+    end
+
+    describe '#delete_user' do
+      let(:user) { BrickFTP::API::User.new(id: 1) }
+      it 'delegate BrickFTP::API::User#destroy' do
+        expect(user).to receive(:destroy)
+        described_class.new.delete_user(user)
+      end
+    end
+  end
 end
