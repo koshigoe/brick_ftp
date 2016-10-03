948f422cf1286b869522cee4824f963aeabf0208
koshigoe <koshigoeb@gmail.com>
2016-10-03 22:52:18 +0900

    Add APIPath to manage API path.

---
+++ lib/brick_ftp/api_component.rb
@@ -0,0 +1,79 @@
+module BrickFTP
+  class APIComponent
+    attr_reader :path_template, :query_keys
+
+    def initialize(path_template, query_keys = [])
+      @path_template = path_template
+      @query_keys = query_keys.map(&:to_sym)
+    end
+
+    def path(params)
+      params = normalize_params(params)
+
+      path_params = build_path_params_from(params)
+      escaped_path_params = path_params.each_with_object({}) do |(k, v), res|
+        res[k] = CGI.escape(v.to_s)
+      end
+
+      path = path_template % escaped_path_params
+      query = build_query_string_from(params)
+      query.empty? ? path : "#{path}?#{query}"
+    end
+
+    def build_path_params_from(params = {})
+      params = normalize_params(params)
+
+      path_variables.each_with_object({}) do |key, res|
+        res[key] = params[key]
+      end
+    end
+
+    def build_query_params_from(params = {})
+      params = normalize_params(params)
+
+      query_keys.each_with_object({}) do |name, res|
+        next unless params.key?(name)
+        res[name] = params[name]
+      end
+    end
+
+    def build_query_string_from(params = {})
+      params = normalize_params(params)
+
+      pairs = build_query_params_from(params).each_with_object([]) do |(k, v), res|
+        res << "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"
+      end
+      pairs.join('&')
+    end
+
+    def except_path_and_query(params = {})
+      params = normalize_params(params)
+
+      exceptions = path_variables + query_keys
+      params.reject { |k, _| exceptions.include?(k) }
+    end
+
+    private
+
+    def path_variables
+      @path_variables ||= path_template.scan(/%\{([^}]+)\}/).map { |m| m.first.to_sym }
+    end
+
+    def normalize_params(params = {})
+      case params
+      when Hash
+        params.symbolize_keys
+      when BrickFTP::API::Base
+        keys = path_variables + query_keys
+        keys.each_with_object({}) do |key, res|
+          res[key] = params.send(key)
+        end
+      else
+        keys = path_variables + query_keys
+        keys.each_with_object({}) do |key, res|
+          res[key] = params
+        end
+      end
+    end
+  end
+end
