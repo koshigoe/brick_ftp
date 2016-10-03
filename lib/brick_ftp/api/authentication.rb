315bb34abc26c3cc826d47c33a6a39cb63a7316d
koshigoe <koshigoeb@gmail.com>
2016-10-03 15:07:32 +0900

    Add API doc.

--- lib/brick_ftp/api/authentication.rb
+++ lib/brick_ftp/api/authentication.rb
@@ -5,14 +5,21 @@ module BrickFTP
     module Authentication
       COOKIE_NAME = 'BrickFTP'.freeze
 
+      # Generate authentication cookie.
+      # @param session [BrickFTP::API::Authentication::Session] authentication session object.
+      # @return [CGI::Cookie] authentication cookie.
       def self.cookie(session)
         CGI::Cookie.new(COOKIE_NAME, session.id)
       end
 
+      # Alias for `BrickFTP::API::Authentication::Session.create`.
+      # @param username [String] username of BrickFTP's user.
+      # @param password [String] password of BrickFTP's user.
       def self.login(username, password)
         Session.create(username: username, password: password)
       end
 
+      # Alias for `BrickFTP::API::Authentication::Session#destroy`.
       def self.logout
         BrickFTP.config.session.destroy
       end
