d50cc48997d4d61065bf1476dcab8dc282c8d754
koshigoe <koshigoeb@gmail.com>
2016-10-03 15:02:55 +0900

    Add API doc

--- lib/brick_ftp/configuration.rb
+++ lib/brick_ftp/configuration.rb
@@ -2,9 +2,17 @@ require 'logger'
 
 module BrickFTP
   class Configuration
+    # Subdomain of API endpoint. If not specified, get value from enviroment variable `BRICK_FTP_SUBDOMAIN`.
+    # @return [String] subdomain
     attr_accessor :subdomain
+    # Access key of API. If not specified, get value from enviroment variable `BRICK_FTP_API_KEY`.
+    # @return [String] API access key
     attr_accessor :api_key
+    # Authentication session for cookie auth.
+    # @return [BrickFTP::API::Authentication::Session] authentication session object.
     attr_accessor :session
+    # Logger.
+    # @return [Logger] logger
     attr_accessor :logger
 
     def initialize
@@ -16,6 +24,8 @@ module BrickFTP
       logger.level = Logger::WARN
     end
 
+    # Host name of API endpoint.
+    # @return [String] API host
     def api_host
       "#{subdomain}.brickftp.com"
     end
