3e6cbc26da634b825c70948ff43168363bbbe69b
koshigoe <koshigoeb@gmail.com>
2016-10-04 23:31:11 +0900

    Add log formatter.

--- lib/brick_ftp.rb
+++ lib/brick_ftp.rb
@@ -1,6 +1,7 @@
 require 'deep_hash_transform'
 require "brick_ftp/version"
 require 'brick_ftp/configuration'
+require 'brick_ftp/log_formatter'
 require 'brick_ftp/http_client'
 require 'brick_ftp/client'
 require 'brick_ftp/api'
---
+++ lib/brick_ftp/log_formatter.rb
@@ -0,0 +1,15 @@
+module BrickFTP
+  class LogFormatter < Logger::Formatter
+    FORMAT = "severity:%{severity}\tpid:%{pid}\ttime:%{time}\tmessage:%{message}\n".freeze
+
+    def call(severity, time, program_name, message)
+      params = {
+        severity: severity,
+        pid: Process.pid,
+        time: format_datetime(time),
+        message: msg2str(message),
+      }
+      FORMAT % params
+    end
+  end
+end
