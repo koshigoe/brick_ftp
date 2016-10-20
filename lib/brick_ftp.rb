486e5e7d089bf694657e6ceabf59c80df66ff38a
koshigoe <koshigoeb@gmail.com>
2016-10-20 18:24:20 +0900

    Add IP address list.

--- lib/brick_ftp.rb
+++ lib/brick_ftp.rb
@@ -37,6 +37,38 @@ require 'brick_ftp/webhook'
 require 'brick_ftp/webhook/request'
 
 module BrickFTP
+  # https://brickftp.com/redundancy/
+  IP_ADDRESSES = %w(
+    54.193.69.72
+    54.193.69.200
+    54.193.65.189
+    54.193.69.198
+    54.208.20.30
+    54.209.242.244
+    54.209.231.233
+    54.208.198.60
+    54.209.231.99
+    54.209.246.178
+    54.209.91.52
+    54.208.63.151
+    54.209.246.217
+    54.209.222.205
+    54.208.169.75
+    52.8.210.89
+    52.74.166.120
+    52.64.2.88
+    52.17.96.203
+    52.28.101.76
+    54.232.253.47
+    54.64.240.152
+    52.74.188.115
+    52.64.6.120
+    52.18.87.39
+    52.29.176.178
+    54.207.27.239
+    52.68.4.44
+  ).freeze
+
   # Return configuration.
   # If it has not been configured yet, initialize configuration.
   # @return [BrickFTP::Configuration] configuration object.
