7345503b7e66690c09bed8983f873dec0fc14254
koshigoe <koshigoeb@gmail.com>
2018-01-16 22:42:56 +0900

    Rubocop: Correct offence about `Lint/UnusedMethodArgument`.

--- lib/brick_ftp/log_formatter.rb
+++ lib/brick_ftp/log_formatter.rb
@@ -2,7 +2,7 @@ module BrickFTP
   class LogFormatter < Logger::Formatter
     FORMAT = "severity:%{severity}\tpid:%{pid}\ttime:%{time}\tmessage:%{message}\n".freeze
 
-    def call(severity, time, program_name, message)
+    def call(severity, time, _program_name, message)
       params = {
         severity: severity,
         pid: Process.pid,
