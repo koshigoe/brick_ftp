c37cd30512ca79400b0c7956ce58b1bae3101726
koshigoe <koshigoeb@gmail.com>
2018-01-17 16:00:23 +0900

    Allow to multi part upload.

--- lib/brick_ftp/api/file_operation/upload.rb
+++ lib/brick_ftp/api/file_operation/upload.rb
@@ -31,17 +31,34 @@ module BrickFTP
         attribute :part, writable: true
         attribute :restart, writable: true
 
-        def self.create(path:, source:)
+        # Upload file.
+        #
+        # @see https://brickftp.com/docs/rest-api/file-uploading/
+        # @param [String] path Remote file path.
+        # @param [IO] source Source `data` (not `path`) to upload.
+        # @param [Integer] chunk_size Size of chunk to multi-part upload.
+        # @return [BrickFTP::API::FileOperation::Upload] An instance of BrickFTP::API::FileOperation::Upload.
+        #
+        def self.create(path:, source:, chunk_size: nil)
           api_client = BrickFTP::HTTPClient.new
-          step1 = api_client.post(api_path_for(:create, path: path), params: { action: 'put' })
+          chunk_io = BrickFTP::Utils::ChunkIO.new(source, chunk_size: chunk_size)
 
-          upload_uri = URI.parse(step1['upload_uri'])
-          upload_client = BrickFTP::HTTPClient.new(upload_uri.host)
-          upload_client.put(step1['upload_uri'], params: source)
+          ref = nil
+          params_for_request_upload_url = { action: 'put' }
+          upload_info = {}
+          chunk_io.each.with_index(1) do |chunk, part|
+            params_for_request_upload_url.update(part: part, ref: ref) if part > 1
+            upload_info = api_client.post(api_path_for(:create, path: path), params: params_for_request_upload_url)
+            ref = upload_info['ref']
 
-          step3 = api_client.post(api_path_for(:create, path: path), params: { action: 'end', ref: step1['ref'] })
+            upload_uri = URI.parse(upload_info['upload_uri'])
+            upload_client = BrickFTP::HTTPClient.new(upload_uri.host)
+            upload_client.put(upload_info['upload_uri'], params: chunk)
+          end
 
-          new(step1.merge(step3).symbolize_keys)
+          uploaded_info = api_client.post(api_path_for(:create, path: path), params: { action: 'end', ref: ref })
+
+          new(upload_info.merge(uploaded_info).symbolize_keys)
         end
       end
     end
