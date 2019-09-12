a8541e3854eb62446522b163b4e9dfdcfcaa7376
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-12 19:42:27 +0900

    feat: update type of Group object.

--- lib/brick_ftp/types/group.rb
+++ lib/brick_ftp/types/group.rb
@@ -4,27 +4,27 @@ module BrickFTP
   module Types
     using BrickFTP::CoreExt::Struct
 
-    # The group object
+    # The Group object
     #
-    # @see https://developers.files.com/#the-group-object The group object
+    # @see https://developers.files.com/#the-group-object The Group object
     #
-    # ATTRIBUTE | TYPE                     | DESCRIPTION
-    # --------- | ------------------------ | -----------
-    # id        | integer                  | Globally unique identifier of each group. Each group is given an ID automatically upon creation.
-    # name      | string                   | Name of the group. This is how the group will be displayed on the site. Maximum of 50 characters.
-    # notes     | text                     | You may use this property to store any additional information you require. There are no restrictions on its formatting.
-    # user_ids  | comma-separated integers | IDs of the users that are in this group.
-    # usernames | string                   | Usernames of the users that are in this group.
-    # admin_ids | comma-separated integers | IDs of the users that are in this group and are administrators of this group.
+    # ATTRIBUTE | TYPE    | DESCRIPTION
+    # --------- | ------- | -----------
+    # id        | integer | Group ID
+    # admin_ids | array   | List of user IDs who are group administrators
+    # name      | string  | Group name
+    # notes     | string  | Notes about this group
+    # user_ids  | array   | List of user IDs who belong to this group
+    # usernames | array   | List of usernames who belong to this group
     #
     Group = Struct.new(
       'Group',
       :id,
+      :admin_ids,
       :name,
       :notes,
       :user_ids,
       :usernames,
-      :admin_ids,
       keyword_init: true
     ) do
       prepend IgnoreUndefinedAttributes
