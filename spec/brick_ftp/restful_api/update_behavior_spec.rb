3c31efb2842721d233cb9a012fe59c458eb950f4
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-13 00:09:58 +0900

    test: update example of UpdateBehavior.

--- spec/brick_ftp/restful_api/update_behavior_spec.rb
+++ spec/brick_ftp/restful_api/update_behavior_spec.rb
@@ -7,27 +7,29 @@ RSpec.describe BrickFTP::RESTfulAPI::UpdateBehavior, type: :lib do
     context 'given correct parameters' do
       it 'return updated Behavior object' do
         updated_behavior = BrickFTP::Types::Behavior.new(
-          id:  38,
-          path: 'a',
+          id: 1,
           behavior: 'webhook',
-          value: %w[https://a.mywebhookhandler.com]
+          path: 'webhook',
+          value: {
+            'method' => 'GET',
+          }
         )
 
-        stub_request(:patch, 'https://subdomain.files.com/api/rest/v1/behaviors/38.json')
+        stub_request(:patch, 'https://subdomain.files.com/api/rest/v1/behaviors/1234.json')
           .with(
             basic_auth: %w[api-key x],
             headers: {
               'User-Agent' => 'BrickFTP Client/1.0 (https://github.com/koshigoe/brick_ftp)',
             },
-            body: { value: %w[https://a.mywebhookhandler.com] }.to_json
+            body: { value: 1 }.to_json
           )
           .to_return(body: updated_behavior.to_h.to_json)
 
         rest = BrickFTP::RESTfulAPI::Client.new('subdomain', 'api-key')
-        params = BrickFTP::RESTfulAPI::UpdateBehavior::Params.new(value: %w[https://a.mywebhookhandler.com])
+        params = BrickFTP::RESTfulAPI::UpdateBehavior::Params.new(value: 1)
         command = BrickFTP::RESTfulAPI::UpdateBehavior.new(rest)
 
-        expect(command.call(38, params)).to eq updated_behavior
+        expect(command.call(1234, params)).to eq updated_behavior
       end
     end
   end
