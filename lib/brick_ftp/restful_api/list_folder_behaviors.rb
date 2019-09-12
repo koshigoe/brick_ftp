af61166cbbefd253c082cf44a8b56301ce301751
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-12 23:51:20 +0900

    feat: update ListFolderBehaviors.

--- lib/brick_ftp/restful_api/list_folder_behaviors.rb
+++ lib/brick_ftp/restful_api/list_folder_behaviors.rb
@@ -4,33 +4,40 @@ require 'erb'
 
 module BrickFTP
   module RESTfulAPI
-    # List behaviors for a folder
+    # List behaviors by path
     #
-    # @see https://developers.files.com/#list-behaviors-for-a-folder List behaviors for a folder
+    # @see https://developers.files.com/#list-behaviors-by-path List behaviors by path
     #
     # ### Params
     #
     # PARAMETER | TYPE     | DESCRIPTION
     # --------- | -------- | -----------
-    # recursive | integer  | Optionally set to 1 to have response include behaviors inherited from parent folders.
+    # path      | string   | Required: Folder behaviors path.
+    # recursive | string   | Show behaviors below this path?
+    # behavior  | string   | If set only shows folder behaviors matching this behavior type.
     #
     class ListFolderBehaviors
       include Command
+      using BrickFTP::CoreExt::Struct
       using BrickFTP::CoreExt::Hash
 
-      # Returns the behaviors that apply to the given path.
+      Params = Struct.new(
+        'ListFolderBehaviorsParams',
+        :behavior,
+        :recursive,
+        keyword_init: true
+      )
+
+      # List behaviors by path
       #
-      # By default, only behaviors applied directly on the the given path will be returned.
-      # If you would also like behaviors that are inherited from parent folders to be returned,
-      # the recursive query parameter can be passed in on the URL with the value of 1.
-      #
-      # @param [String] path
-      # @param [Boolean] recursive
+      # @param [String] path Folder behaviors path.
+      # @param [BrickFTP::RESTfulAPI::ListFolderBehaviors::Params] params parameters for search folder behaviors
       # @return [Array<BrickFTP::Types::Behavior>] Behaviors
       #
-      def call(path, recursive: false)
+      def call(path, params)
         endpoint = "/api/rest/v1/behaviors/folders/#{ERB::Util.url_encode(path)}"
-        endpoint = "#{endpoint}?recursive=1" if recursive
+        query = params.to_h.compact.sort.map { |k, v| "#{k}=#{ERB::Util.url_encode(v.to_s)}" }.join('&')
+        endpoint += "?#{query}" unless query.empty?
         res = client.get(endpoint)
 
         res.map { |i| BrickFTP::Types::Behavior.new(i.symbolize_keys) }
--- spec/brick_ftp/restful_api/list_folder_behaviors_spec.rb
+++ spec/brick_ftp/restful_api/list_folder_behaviors_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe BrickFTP::RESTfulAPI::ListFolderBehaviors, type: :lib do
     context 'correct request' do
       it 'return Array of Behavior object' do
         expected_behavior = BrickFTP::Types::Behavior.new(
-          id:  38,
-          path: 'Finance',
+          id: 1,
           behavior: 'webhook',
-          value: %w[https://a.mywebhookhandler.com]
+          path: 'webhook',
+          value: {
+            'method' => 'GET',
+          }
         )
 
-        stub_request(:get, 'https://subdomain.files.com/api/rest/v1/behaviors/folders/a?recursive=1')
+        stub_request(:get, 'https://subdomain.files.com/api/rest/v1/behaviors/folders/a?behavior=webhook&recursive=true')
           .with(
             basic_auth: %w[api-key x],
             headers: {
@@ -23,9 +25,10 @@ RSpec.describe BrickFTP::RESTfulAPI::ListFolderBehaviors, type: :lib do
           .to_return(body: [expected_behavior.to_h].to_json)
 
         rest = BrickFTP::RESTfulAPI::Client.new('subdomain', 'api-key')
+        params = BrickFTP::RESTfulAPI::ListFolderBehaviors::Params.new(recursive: true, behavior: 'webhook')
         command = BrickFTP::RESTfulAPI::ListFolderBehaviors.new(rest)
 
-        expect(command.call('a', recursive: true)).to eq([expected_behavior])
+        expect(command.call('a', params)).to eq([expected_behavior])
       end
     end
   end
