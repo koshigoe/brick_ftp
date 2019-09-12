025c07d6851407059d87980591a6e608fae44027
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-12 19:35:31 +0900

    feat: update type of File object.

--- lib/brick_ftp/types/file.rb
+++ lib/brick_ftp/types/file.rb
@@ -4,25 +4,28 @@ module BrickFTP
   module Types
     using BrickFTP::CoreExt::Struct
 
-    # The file/folder object
+    # The File object
     #
-    # @see https://developers.files.com/#the-file-folder-object The file/folder object
+    # @see https://developers.files.com/#the-file-object The File object
     #
     # ATTRIBUTE          | TYPE     | DESCRIPTION
     # ------------------ | -------- | -----------
-    # id                 | integer  | Globally unique identifier of each file.
-    # path               | string   | Full path of the file or folder. Maximum of 550 characters.
-    # display_name       | string   | Name of the file or folder without path.
-    # type               | enum     | Either `file` or `directory` (meaning folder).
-    # size               | integer  | Size of the file in bytes. Will be 0 for a folder.
-    # mtime              | datetime | Date of the file/folder's last modification.
-    # provided_mtime     | datetime | Client-provided date of the file/folder's last modification.
-    # crc32              | string   | CRC32 for the file, if available.
-    # md5                | string   | MD5 hash for the file, if available.
-    # region             | string   | The geographic region that a file is stored in.
-    # permissions        | string   | Your permissions on the file/folder. Will be one of the following:<br>`p` (list/preview only), `r` (read-only), `w` (write-only), `rw` (read/write), `rwd` (read/write/delete).
-    # download_uri       | string   | URL to download the file, if requested.
-    # subfolders_locked? | integer  | A value of `1` indicates that the Lock-subfolders setting is enabled on a folder.
+    # id                 | integer  | File/Folder ID
+    # path               | string   | File/Folder path This must be slash-delimited, but it must neither start nor end with a slash. Maximum of 5000 characters.
+    # display_name       | string   | File/Folder display name
+    # type               | string   | Type: `directory` or `file`.
+    # size               | integer  | File/Folder size
+    # mtime              | datetime | File last modified date/time, according to the server. This is the timestamp of the last Files.com operation of the file, regardless of what modified timestamp was sent.
+    # provided_mtime     | datetime | File last modified date/time, according to the client who set it. Files.com allows desktop, FTP, SFTP, and WebDAV clients to set modified at times. This allows Desktop<->Cloud syncing to preserve modified at times.
+    # crc32              | string   | File CRC32 checksum. This is sometimes delayed, so if you get a blank response, wait and try again.
+    # md5                | string   | File MD5 checksum. This is sometimes delayed, so if you get a blank response, wait and try again.
+    # region             | string   | Region location
+    # permissions        | string   | A short string representing the current user's permissions. Can be `r`,`w`,`p`, or any combination
+    # subfolders_locked? | boolean  | Are subfolders locked and unable to be modified?
+    # download_uri       | string   | Link to download file. Provided only in response to a download request.
+    # priority_color     | string   | Bookmark/priority color of file/folder
+    # preview_id         | integer  | File preview ID
+    # preview            | ?        | File preview
     #
     File = Struct.new(
       'File',
@@ -37,8 +40,11 @@ module BrickFTP
       :md5,
       :region,
       :permissions,
-      :download_uri,
       :subfolders_locked?,
+      :download_uri,
+      :priority_color,
+      :preview_id,
+      :preview,
       keyword_init: true
     ) do
       prepend IgnoreUndefinedAttributes
