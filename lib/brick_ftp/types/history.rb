85e424baa5954ee3caec2eb7f32e4d692751aeac
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-12 19:47:01 +0900

    feat: update type of History object.

--- lib/brick_ftp/types/history.rb
+++ lib/brick_ftp/types/history.rb
@@ -4,38 +4,40 @@ module BrickFTP
   module Types
     using BrickFTP::CoreExt::Struct
 
-    # The notification object
+    # The History object
     #
-    # @see https://developers.files.com/#the-notification-object The notification object
+    # @see https://developers.files.com/#the-history-object The History object
     #
     # ATTRIBUTE     | TYPE     | DESCRIPTION
     # ------------- | -------- | -----------
-    # id            | integer  | Globally unique identifier of each history entry.
-    # when          | datetime | Date of the history entry.
-    # user_id       | integer  | ID of the user associated with the history entry.
-    # username      | string   | Username of the user associated with the history entry.
-    # action        | string   | Type of action that occurred. Will be one of the following: <br>`create`, `read`, `update`, `destroy`, `move`, `login`, `failedlogin`, `copy`, `user_create`, `user_destroy`, `group_create`, `group_destroy`, `permission_create`, `permission_destroy`.
-    # failure_type  | string   | Type of failure that occurred, if any.
-    # path          | string   | Path of the file or folder associated with the history entry.
-    # source        | string   | Source path associated with the history entry.
-    # destination   | string   | Destination path associated with the history entry.
-    # targets       | object   | Object containing the target object(s) for `user_create`, `user_destroy`, `group_create`, `group_destroy`, `permission_create`, and `permission_destroy` actions. <br>A `user` target will include an `id` and `username`. A `group` target will include an `id` and `name`. A `permission` target will include an `id`, `permission`, and a `recursive` parameter.
-    # ip            | string   | IP address associated with the history entry.
-    # interface     | string   | Interface associated with the history entry. Will be one of the following: web, ftp, robot, jsapi, restapi, sftp, dav.
+    # id            | integer  | Action ID
+    # when          | datetime | Action occurrence date/time
+    # destination   | string   | The destination path for this action, if applicable
+    # display       | string   | Display format
+    # ip            | string   | IP Address that performed this action
+    # path          | string   | Path This must be slash-delimited, but it must neither start nor end with a slash. Maximum of 5000 characters.
+    # source        | string   | The source path for this action, if applicable
+    # targets       | array    | Targets
+    # user_id       | integer  | User ID
+    # username      | string   | Username
+    # action        | string   | Type of action. Can be `create`, `read`, `update`, `destroy`, `move`, `login`, `failedlogin`, `copy`, `user_create`, `user_update`, `user_destroy`, `group_create`, `group_update`, `group_destroy`, `permission_create`, `permission_destroy`, `api_key_create`, `api_key_update`, or `api_key_destroy`
+    # failure_type  | string   |Failure type. If action was a user login or session failure, why did it fail? Can be `expired_trial`, `account_overdue`, `locked_out`, `ip_mismatch`, `password_mismatch`, `site_mismatch`, `username_not_found`, `none`, `no_ftp_permission`, `no_web_permission`, `no_directory`, `errno_enoent`, `no_sftp_permission`, `no_dav_permission`, `no_restapi_permission`, `key_mismatch`, `region_mismatch`, or `expired_access`
+    # interface     | string   | Interface on which this action occurred.
     #
     History = Struct.new(
       'History',
       :id,
       :when,
+      :destination,
+      :display,
+      :ip,
+      :path,
+      :source,
+      :targets,
       :user_id,
       :username,
       :action,
       :failure_type,
-      :path,
-      :source,
-      :destination,
-      :targets,
-      :ip,
       :interface,
       keyword_init: true
     ) do
