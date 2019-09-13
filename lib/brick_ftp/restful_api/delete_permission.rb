89c8701c4d2871eab55727a77d747f024a47c537
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-13 21:56:09 +0900

    docs: update document of DeletePermission.

--- lib/brick_ftp/restful_api/delete_permission.rb
+++ lib/brick_ftp/restful_api/delete_permission.rb
@@ -2,17 +2,16 @@
 
 module BrickFTP
   module RESTfulAPI
-    # Delete a permission
+    # Delete permission
     #
-    # @see https://developers.files.com/#delete-a-permission Delete a permission
+    # @see https://developers.files.com/#delete-permission Delete permission
     #
     class DeletePermission
       include Command
 
-      # Deletes the specified group.
+      # Delete permission
       #
-      # @param [Integer] id Globally unique identifier of each permission.
-      #   Each permission is given an ID automatically upon creation.
+      # @param [Integer] id Permission ID.
       #
       def call(id)
         client.delete("/api/rest/v1/permissions/#{id}.json")
