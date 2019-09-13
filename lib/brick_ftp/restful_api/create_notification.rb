d355a68fd4de21f8e082565abc7a83a4b9124615
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-13 21:31:33 +0900

    feat: update CreateNotification.

--- lib/brick_ftp/restful_api/create_notification.rb
+++ lib/brick_ftp/restful_api/create_notification.rb
@@ -2,18 +2,21 @@
 
 module BrickFTP
   module RESTfulAPI
-    # Create a notification
+    # Create notification
     #
-    # @see https://developers.files.com/#create-a-notification Create a notification
+    # @see https://developers.files.com/#create-notification Create notification
     #
     # ### Params
     #
-    # PARAMETER     | TYPE    | DESCRIPTION
-    # ------------- | ------- | -----------
-    # path          | string  | Folder path for notifications. This must be slash-delimited, but it must neither start nor end with a slash. Maximum of 5000 characters.
-    # user_id       | integer | Unique identifier for the user being notified. Each user is given an ID automatically upon creation. You can look up user IDs by using the User resource of this REST API.
-    # username      | string  | Username for the user given by user_id. If this value is set during creation and user_id is not set, the user_id is looked up from the username and set. Maximum of 50 characters.
-    # send_interval | string  | The send interval for notifications. Can be one of the following: five_minutes (default), fifteen_minutes, hourly, daily.
+    # PARAMETER           | TYPE    | DESCRIPTION
+    # ------------------- | ------- | -----------
+    # group_id            | integer | The ID of the group to notify. Provide user_id, username or group_id.
+    # notify_on_copy      | boolean | If true, copying or moving resources into this path will trigger a notification, in addition to just uploads.
+    # notify_user_actions | boolean | If true actions initiated by the user will still result in a notification
+    # path                | string  | Path
+    # send_interval       | string  | The time interval that notifications are aggregated by. Can be five_minutes, fifteen_minutes, hourly, or daily.
+    # user_id             | integer | The id of the user to notify. Provide user_id, username or group_id.
+    # username            | string  | The username of the user to notify. Provide user_id, username or group_id.
     #
     class CreateNotification
       include Command
@@ -22,17 +25,20 @@ module BrickFTP
 
       Params = Struct.new(
         'CreateNotificationParams',
+        :group_id,
+        :notify_on_copy,
+        :notify_user_actions,
         :path,
+        :send_interval,
         :user_id,
         :username,
-        :send_interval,
         keyword_init: true
       )
 
-      # Creates a new notification on the current site.
+      # Create notification
       #
       # @param [BrickFTP::RESTfulAPI::CreateNotification::Params] params parameters
-      # @return [BrickFTP::Types::Notification] Notification
+      # @return [BrickFTP::Types::Notification]
       #
       def call(params)
         res = client.post('/api/rest/v1/notifications.json', params.to_h.compact)
--- spec/brick_ftp/restful_api/create_notification_spec.rb
+++ spec/brick_ftp/restful_api/create_notification_spec.rb
@@ -7,12 +7,17 @@ RSpec.describe BrickFTP::RESTfulAPI::CreateNotification, type: :lib do
     context 'given correct parameters' do
       it 'return created Notification object' do
         created_notification = BrickFTP::Types::Notification.new(
-          id: 1234,
-          path: 'a/b/c',
-          user_id: '1',
-          username: 'user',
-          send_interval: 'daily',
-          unsubscribed: false
+          id: 1,
+          group_id: 1,
+          group_name: '',
+          notify_user_actions: true,
+          notify_on_copy: true,
+          path: 'path',
+          send_interval: 'fifteen_minutes',
+          unsubscribed: true,
+          unsubscribed_reason: '',
+          user_id: 1,
+          username: 'User'
         )
 
         stub_request(:post, 'https://subdomain.files.com/api/rest/v1/notifications.json')
@@ -21,12 +26,28 @@ RSpec.describe BrickFTP::RESTfulAPI::CreateNotification, type: :lib do
             headers: {
               'User-Agent' => 'BrickFTP Client/1.0 (https://github.com/koshigoe/brick_ftp)',
             },
-            body: { path: 'a/b/c', user_id: 1 }.to_json
+            body: {
+              group_id: 1,
+              notify_on_copy: true,
+              notify_user_actions: true,
+              path: 'path',
+              send_interval: 'daily',
+              user_id: 1,
+              username: 'User',
+            }.to_json
           )
           .to_return(body: created_notification.to_h.to_json)
 
         rest = BrickFTP::RESTfulAPI::Client.new('subdomain', 'api-key')
-        params = BrickFTP::RESTfulAPI::CreateNotification::Params.new(path: 'a/b/c', user_id: 1)
+        params = BrickFTP::RESTfulAPI::CreateNotification::Params.new(
+          group_id: 1,
+          notify_on_copy: true,
+          notify_user_actions: true,
+          path: 'path',
+          send_interval: 'daily',
+          user_id: 1,
+          username: 'User'
+        )
         command = BrickFTP::RESTfulAPI::CreateNotification.new(rest)
 
         expect(command.call(params)).to eq created_notification
