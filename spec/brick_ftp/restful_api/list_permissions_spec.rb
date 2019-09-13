7f42b6f2bf426c1e24049ba1741f52e5f2250e7e
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-13 21:36:30 +0900

    test: update example of ListPermissions.

--- spec/brick_ftp/restful_api/list_permissions_spec.rb
+++ spec/brick_ftp/restful_api/list_permissions_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe BrickFTP::RESTfulAPI::ListPermissions, type: :lib do
     context 'correct request' do
       it 'return Array of Permission object' do
         expected_permission = BrickFTP::Types::Permission.new(
-          id: 1234,
-          user_id: 1234,
-          username: 'user',
-          group_id: 1234,
-          group_name: 'group',
-          path: 'path',
+          id: 1,
+          user_id: 1,
+          username: 'Sser',
+          group_id: 1,
+          group_name: '',
+          path: '',
           permission: 'full',
           recursive: true
         )
