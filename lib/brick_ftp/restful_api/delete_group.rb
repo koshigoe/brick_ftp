0408a8cb315a04b7bf1c6689a390c6780cc8949c
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-13 17:37:31 +0900

    docs: update document of DeleteGroup.

--- lib/brick_ftp/restful_api/delete_group.rb
+++ lib/brick_ftp/restful_api/delete_group.rb
@@ -2,17 +2,16 @@
 
 module BrickFTP
   module RESTfulAPI
-    # Delete a group
+    # Delete group
     #
-    # @see https://developers.files.com/#delete-a-group Delete a group
+    # @see https://developers.files.com/#delete-group Delete group
     #
     class DeleteGroup
       include Command
 
-      # Deletes the specified group.
+      # Delete group
       #
-      # @param [Integer] id Globally unique identifier of each group.
-      #   Each group is given an ID automatically upon creation.
+      # @param [Integer] id Group ID.
       #
       def call(id)
         client.delete("/api/rest/v1/groups/#{id}.json")
