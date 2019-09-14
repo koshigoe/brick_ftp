a5d1a35bdfe82f976abcdee3da4e3152d3310804
Masataka SUZUKI <koshigoeb@gmail.com>
2019-09-14 18:40:22 +0900

    feat: update UpdateUser.

--- lib/brick_ftp/restful_api/update_user.rb
+++ lib/brick_ftp/restful_api/update_user.rb
@@ -2,41 +2,45 @@
 
 module BrickFTP
   module RESTfulAPI
-    # Update a user
+    # Update user
     #
-    # @see https://developers.files.com/#update-a-user Update a user
+    # @see https://developers.files.com/#update-user Update user
     #
     # ### Params
     #
-    # PARAMETER               | TYPE                     | DESCRIPTION
-    # ----------------------- | ------------------------ | -----------
-    # username                | string                   | Username for the user. This is how the user will be displayed on the site. Maximum of 50 characters.
-    # authentication_method   | enum                     | Authentication method for this user. Can be one of the following: password, ldap, or oauth_google. Default is password.
-    # password                | string                   | Password for the user. This property is write-only. It cannot be retrieved via the API.
-    # password_confirmation   | string                   | Confirms the new password provided in the password field. This field is optional but will be validated if provided.
-    # authenticate_until      | datetime                 | If set, the user will be blocked from logging in after this date.
-    # name                    | string                   | Real name of the user. For your reference. Maximum of 50 characters.
-    # email                   | string                   | E-Mail address of the user. Maximum of 50 characters.
-    # notes                   | text                     | You may use this property to store any additional information you require. There are no restrictions on its formatting.
-    # group_ids               | comma-separated integers | IDs of the Groups that this user is in.
-    # ftp_permission          | boolean                  | Allow user access via FTP/FTPS (port 21 or 990) interface. Default is true.
-    # sftp_permission         | boolean                  | Allow user access via SFTP (port 22) interface. Default is true.
-    # dav_permission          | boolean                  | Allow user access via WebDAV (port 443) interface. Default is true.
-    # restapi_permission      | boolean                  | Allow user access the REST API, via HTTP/HTTPS (port 80 or 443), and the desktop application. Default is true.
-    # attachments_permission  | boolean                  | Allow user to use Sharing tab in web interface to share files with anonymous users via a unique URL. Default is false.
-    # self_managed            | boolean                  | Allow user to change their password and user information via the web interface. Default is true.
-    # require_password_change | boolean                  | Require user to change their password at their next login. Note: requires restapi_permission to be true, as password changes can only occur via the web interface. Default is false.
-    # allowed_ips             | text                     | List allowed IPs, one per line. You may specify a range in CIDR format, such as 192.168.1.0/27. Leave blank to allow all IPs. If you are also restricting IP addresses on the Site tab, users matching in either place will be allowed to log in.
-    # user_root               | string                   | Folder to show as the root when this user logs in via the FTP interface. Make sure this folder exists, as it will not be automatically created. Does not apply to the web interface! This should not contain a leading slash, but must contain a trailing slash. Example: Users/jenny/. Limit of 250 characters.
-    # time_zone               | string                   | File modification times will be displayed in this time zone. Default is Eastern Time (US & Canada).
-    # language                | string                   | The language that BrickFTP will be displayed in, if the translation is available. Leave as default (null) to auto-detect or use the site setting.
-    # grant_permission        | enum                     | Value must be set to full, read, write, preview, read+write, or preview+write. The user will be granted that permission on their FTP root folder as defined by the user_root. This property is write-only. It cannot be retrieved via the User resource of the REST API, though may be obtained with the Permissions resource of the REST API.
-    # ssl_required            | enum                     | Whether or not SSL encryption is required on all connections for this user. Can be one of the following: use_system_setting, always_require, or never_require. Default is use_system_setting.
-    # site_admin              | boolean                  | If set to true, this user will be treated as a site-wide administrator. Default is false.
-    # receive_admin_alerts    | boolean                  | Enable this user to receive alerts sent to site administrators. This property will only be accepted/returned if site_admin is true. Default is false.
-    # subscribe_to_newsletter | boolean                  | Add this user to the BrickFTP newsletter and our mailing list for notices about product updates. This property will only be accepted/returned if site_admin is true. Default is false.
-    # can_create_new_users    | boolean                  | If true, this user will be able to create other users within groups where they have administrator privileges. Default is false.
-    # admin_group_ids         | comma-separated integers | IDs of the Groups that this user is an administrator of.
+    # PARAMETER                    | TYPE    | DESCRIPTION
+    # ---------------------------- | ------- | -----------
+    # id                           | integer | Required: User ID to update.
+    # change_password              | string  | Used for changing a password on an existing user.
+    # change_password_confirmation | string  | Optional, but if provided, we will ensure that it matches the value sent in change_password.
+    # email                        | string  | User's email.
+    # grant_permission             | string  | Permission to grant on the user root. Can be blank or full, read, write, preview, or history.
+    # group_ids                    | string  | A list of group ids to associate this user with. Comma delimited.
+    # password                     | string  | User password.
+    # password_confirmation        | string  | Optional, but if provided, we will ensure that it matches the value sent in password.
+    # allowed_ips                  | string  | A list of allowed IPs if applicable. Newline delimited
+    # attachments_permission       | boolean | Can the user create Bundles (aka Share Links)? This field will be aliased or renamed in the future to bundles_permission.
+    # authenticate_until           | string  | Scheduled Date/Time at which user will be deactivated
+    # authentication_method        | string  | How is this user authenticated? One of password, ldap, or sso
+    # bypass_site_allowed_ips      | boolean | Allow this user to skip site-wide IP blacklists?
+    # dav_permission               | boolean | Can the user connect with WebDAV?
+    # ftp_permission               | boolean | Can the user access with FTP/FTPS?
+    # language                     | string  | Preferred language
+    # name                         | string  | User's full name
+    # notes                        | string  | Any internal notes on the user
+    # password_validity_days       | integer | Number of days to allow user to use the same password
+    # receive_admin_alerts         | boolean | Should the user receive admin alerts such a certificate expiration notifications and overages?
+    # require_password_change      | boolean | Is a password change required upon next user login?
+    # restapi_permission           | boolean | Can this user access the REST API?
+    # self_managed                 | boolean | Does this user manage it's own credentials or is it a shared/bot user?
+    # sftp_permission              | boolean | Can the user access with SFTP?
+    # site_admin                   | boolean | Is the user an administrator for this site?
+    # ssl_required                 | string  | SSL required setting. Can be system_setting, always_require, or never_require.
+    # sso_strategy_id              | integer | SSO (Single Sign On) strategy ID for the user, if applicable.
+    # subscribe_to_newsletter      | boolean | Is the user subscribed to the newsletter?
+    # time_zone                    | string  | User time zone
+    # user_root                    | string  | Root folder for FTP (and optionally SFTP if the appropriate site-wide setting is set.) Note that this is not used for API, Desktop, or Web interface.
+    # username                     | string  | User's username
     #
     class UpdateUser
       include Command
@@ -45,54 +49,48 @@ module BrickFTP
 
       Params = Struct.new(
         'UpdateUserParams',
-        :username,
-        :authentication_method,
+        :id,
+        :change_password,
+        :change_password_confirmation,
+        :email,
+        :grant_permission,
+        :group_ids,
         :password,
         :password_confirmation,
-        :authenticate_until,
-        :name,
-        :email,
-        :notes,
-        :group_ids,
-        :ftp_permission,
-        :sftp_permission,
-        :dav_permission,
-        :restapi_permission,
-        :attachments_permission,
-        :self_managed,
-        :require_password_change,
         :allowed_ips,
-        :user_root,
-        :time_zone,
+        :attachments_permission,
+        :authenticate_until,
+        :authentication_method,
+        :bypass_site_allowed_ips,
+        :dav_permission,
+        :ftp_permission,
         :language,
-        :grant_permission,
-        :ssl_required,
-        :site_admin,
+        :name,
+        :notes,
+        :password_validity_days,
         :receive_admin_alerts,
+        :require_password_change,
+        :restapi_permission,
+        :self_managed,
+        :sftp_permission,
+        :site_admin,
+        :ssl_required,
+        :sso_strategy_id,
         :subscribe_to_newsletter,
-        :can_create_new_users,
-        :admin_group_ids,
+        :time_zone,
+        :user_root,
+        :username,
         keyword_init: true
       )
 
-      # Updates the specified user.
+      # Update user
       #
-      # For additional security, this method requires reauthentication when updating a password unless an API key is used.
+      # @param [BrickFTP::RESTfulAPI::CreateUser::Params] params parameters
+      # @return [BrickFTP::Types::User]
       #
-      # @note The `allowed_ips` may accept only blank value.
-      #
-      #   e.g. When set `"192.168.1.0/27"` to `allowed_ips`, receive following message with 500 error:
-      #
-      #     An unhandled lowlevel error occurred. The application logs may have details.
-      #
-      #
-      # @param [Integer] id Globally unique identifier of each user.
-      #   Each user is given an ID automatically upon creation.
-      # @param [BrickFTP::RESTfulAPI::CreateUser::Params] params parameters for create a user
-      # @return [BrickFTP::Types::User] updated User
-      #
-      def call(id, params)
-        res = client.put("/api/rest/v1/users/#{id}.json", params.to_h.compact)
+      def call(params)
+        params = params.to_h.compact
+        res = client.put("/api/rest/v1/users/#{params.delete(:id)}.json", params)
 
         BrickFTP::Types::User.new(res.symbolize_keys)
       end
--- spec/brick_ftp/restful_api/update_user_spec.rb
+++ spec/brick_ftp/restful_api/update_user_spec.rb
@@ -7,52 +7,106 @@ RSpec.describe BrickFTP::RESTfulAPI::UpdateUser, type: :lib do
     context 'given correct parameters' do
       it 'return updated User object' do
         updated_user = BrickFTP::Types::User.new(
-          id: 1234,
-          username: 'user',
-          authentication_method: 'password',
-          last_login_at: nil,
-          authenticate_until: nil,
-          name: nil,
-          email: 'user@example.com',
-          notes: nil,
-          group_ids: '',
-          ftp_permission: true,
-          sftp_permission: true,
-          dav_permission: true,
-          restapi_permission: true,
+          id: 1,
+          admin_group_ids: [],
+          allowed_ips: '127.0.0.1',
           attachments_permission: true,
-          self_managed: true,
-          require_password_change: false,
-          require_2fa: false,
-          allowed_ips: nil,
-          user_root: '',
-          time_zone: 'Eastern Time (US & Canada)',
-          language: '',
-          ssl_required: 'use_system_setting',
-          site_admin: true,
-          password_set_at: nil,
+          authenticate_until: '2000-01-01 01:00:00 UTC',
+          authentication_method: 'password',
+          bypass_site_allowed_ips: true,
+          dav_permission: true,
+          email: 'john.doe@files.com',
+          ftp_permission: true,
+          group_ids: [],
+          language: 'en',
+          last_login_at: '2000-01-01 01:00:00 UTC',
+          last_protocol_cipher: '',
+          lockout_expires: '2000-01-01 01:00:00 UTC',
+          name: 'John Doe',
+          notes: 'Internal notes on this user.',
+          password_set_at: '2000-01-01 01:00:00 UTC',
+          password_validity_days: 1,
+          public_keys_count: 1,
           receive_admin_alerts: true,
-          subscribe_to_newsletter: false,
-          last_protocol_cipher: nil,
-          lockout_expires: nil,
-          admin_group_ids: []
+          require_2fa: true,
+          require_password_change: true,
+          restapi_permission: true,
+          self_managed: true,
+          sftp_permission: true,
+          site_admin: true,
+          ssl_required: 'always_require',
+          sso_strategy_id: 1,
+          subscribe_to_newsletter: true,
+          externally_managed: true,
+          time_zone: 'Pacific Time (US & Canada)',
+          user_root: '',
+          username: 'user'
         )
 
-        stub_request(:put, 'https://subdomain.files.com/api/rest/v1/users/1234.json')
+        stub_request(:put, 'https://subdomain.files.com/api/rest/v1/users/1.json')
           .with(
             basic_auth: %w[api-key x],
             headers: {
               'User-Agent' => 'BrickFTP Client/1.0 (https://github.com/koshigoe/brick_ftp)',
             },
-            body: { username: 'user' }.to_json
+            body: {
+              email: 'john.doe@files.com',
+              allowed_ips: '127.0.0.1',
+              attachments_permission: true,
+              authenticate_until: '2000-01-01 01:00:00 UTC',
+              authentication_method: 'password',
+              bypass_site_allowed_ips: true,
+              dav_permission: true,
+              ftp_permission: true,
+              language: 'en',
+              name: 'John Doe',
+              notes: 'Internal notes on this user.',
+              password_validity_days: 1,
+              receive_admin_alerts: true,
+              require_password_change: true,
+              restapi_permission: true,
+              self_managed: true,
+              sftp_permission: true,
+              site_admin: true,
+              ssl_required: 'always_require',
+              sso_strategy_id: 1,
+              subscribe_to_newsletter: true,
+              time_zone: 'Pacific Time (US & Canada)',
+              username: 'user',
+            }.to_json
           )
           .to_return(body: updated_user.to_h.to_json)
 
         rest = BrickFTP::RESTfulAPI::Client.new('subdomain', 'api-key')
-        params = BrickFTP::RESTfulAPI::UpdateUser::Params.new(username: 'user')
+        params = BrickFTP::RESTfulAPI::UpdateUser::Params.new(
+          id: 1,
+          email: 'john.doe@files.com',
+          allowed_ips: '127.0.0.1',
+          attachments_permission: true,
+          authenticate_until: '2000-01-01 01:00:00 UTC',
+          authentication_method: 'password',
+          bypass_site_allowed_ips: true,
+          dav_permission: true,
+          ftp_permission: true,
+          language: 'en',
+          name: 'John Doe',
+          notes: 'Internal notes on this user.',
+          password_validity_days: 1,
+          receive_admin_alerts: true,
+          require_password_change: true,
+          restapi_permission: true,
+          self_managed: true,
+          sftp_permission: true,
+          site_admin: true,
+          ssl_required: 'always_require',
+          sso_strategy_id: 1,
+          subscribe_to_newsletter: true,
+          time_zone: 'Pacific Time (US & Canada)',
+          username: 'user'
+        )
         command = BrickFTP::RESTfulAPI::UpdateUser.new(rest)
 
-        expect(command.call(1234, params)).to eq updated_user
+        expect(command.call(params)).to eq updated_user
       end
     end
   end
